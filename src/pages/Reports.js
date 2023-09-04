852b95d2c909eeddcc6c4d8d0df36e1ae52da293
abeer0441 <a.a.abeer4444@gmail.com>
2023-09-04 15:44:32 +0300

    first commit

---
+++ src/pages/Reports.js
@@ -0,0 +1,54 @@
+import { useLocation } from 'react-router-dom';
+import React from 'react';
+import { Link } from 'react-router-dom'
+import img1 from '../img/NWC-Logo.svg';
+
+
+
+
+function Reports() {
+    const locaction = useLocation();
+    const data = locaction.state;
+
+    document.querySelector('html').setAttribute("id", "Reports");
+   
+    const sendDataToStreamlit = (data) => {
+        fetch("http://127.0.0.1:5000/streamlit", {
+          method: "POST",
+          body: JSON.stringify(data),
+          headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
+            'Access-Control-Allow-Headers': 'Content-Type',
+          }
+        });
+    };
+      
+    // Usage example
+    const status_code = sendDataToStreamlit(data)
+    console.log(status_code);
+
+    return (        
+        <div className="Reports">
+          <header> 
+            <nav>
+              <Link to="/" className="home"> Home page </Link>
+              <Link to="/file" className="file"> Import new file </Link> 
+              </nav>
+              <img src={img1} className='logo'/>
+          </header>
+
+          <iframe src="http://localhost:8501/" frameborder="0" 
+          marginheight="0" 
+          marginwidth="0" 
+          width="100%" 
+          height="100%" 
+          title='charts'
+          scrolling="auto"></iframe>
+
+        </div>
+    );
+}
+
+export default Reports;
